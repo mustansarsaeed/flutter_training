7b67c3e9d6dcaf940582b72067a345d90d2ec553
Mustansar Saeed <m.mustansar.saeed@gmail.com>
2022-10-13 10:46:30 +0500

    Login form using Scoped Bloc instances

--- login_bloc/lib/src/app.dart
+++ login_bloc/lib/src/app.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'blocs/provider.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
-    return MaterialApp(
+    return Provider(
+        child: MaterialApp(
       title: 'Log Me in!',
       home: Scaffold(
         body: LoginScreen(),
       ),
-    );
+    ));
   }
 }
--- login_bloc/lib/src/screens/login_screen.dart
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:login_bloc/src/blocs/provider.dart';
 import '../blocs/bloc.dart';
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
+    final bloc = Provider.of(context).bloc;
+
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-          emailField(),
-          passwordField(),
+          emailField(bloc),
+          passwordField(bloc),
           Container(
             margin: EdgeInsets.only(top: 20.0),
           ),
@@ -18,7 +21,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget emailField() {
+  Widget emailField(Bloc bloc) {
     return StreamBuilder(
         stream: bloc.email,
         builder: ((context, snapshot) {
@@ -34,7 +37,7 @@ class LoginScreen extends StatelessWidget {
         }));
   }
 
-  Widget passwordField() {
+  Widget passwordField(Bloc bloc) {
     return StreamBuilder(
         stream: bloc.password,
         builder: (context, snapshot) {
